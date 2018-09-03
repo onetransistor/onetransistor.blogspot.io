ee8dd1b62ab9593c08f577527dc8290f533d25c1
OneTransistor <one1transistor@gmail.com>
2018-09-03 22:03:30 +0300

    Update bgads.js

--- bgads.js
+++ bgads.js
@@ -9,7 +9,7 @@ $.getJSON(au, function(data) {
 function showAd(ad) {
 	var ac = '<a href="' + ad.url + '" target="_blank" rel="nofollow" onclick="gtag_ev(\'TopBanner\')"><img src="' + ad.img + '" /></a>';
 	var ae = document.getElementById("top_banner");
-	if (ae == NULL) return;
+	if (ae == null) return;
 	ae.innerHTML = ac;
 	ae.className = "onetransistor_main_top_banner";
 	ae.setAttribute("data-placement", "bottom");

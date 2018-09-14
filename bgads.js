736586e29b9910202072265209818206cdbf8b00
OneTransistor <one1transistor@gmail.com>
2018-09-14 22:35:48 +0300

    Update bgads.js

--- bgads.js
+++ bgads.js
@@ -1,5 +1,5 @@
 
-var au = "https://gist.githubusercontent.com/onetransistor/b0f91dcc68a3da9e1f899750c682165c/raw/0d7d4b2cfaf44ae4621a454f002c7af9a1d7b847/bg_ads.json"
+var au = "https://software.onetransistor.eu/ads.json"
 $.getJSON(au, function(data) {
 	data.ads.forEach(showAd);
 });
